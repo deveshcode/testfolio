06baae8ceb97d505b1c0760dbaf072bc74d939d3
deveshcode <deveshssurve@gmail.com>
2022-11-18 22:07:21 +0000

    Updates

---
+++ precache-manifest.e516ac50489346e9abca174d4228b5f6.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f3024cea707d0482ad5efefce079d7",
+    "url": "/testfolio/index.html"
+  },
+  {
+    "revision": "075f5a4014c20d002714",
+    "url": "/testfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "a837d200c4102d0b1489",
+    "url": "/testfolio/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "c4b6c8c40d88b33563cb",
+    "url": "/testfolio/static/css/main.be85415d.chunk.css"
+  },
+  {
+    "revision": "aea45f25ae0856ec2452",
+    "url": "/testfolio/static/js/2.8a891f12.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/testfolio/static/js/2.8a891f12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075f5a4014c20d002714",
+    "url": "/testfolio/static/js/3.2694d0f6.chunk.js"
+  },
+  {
+    "revision": "a837d200c4102d0b1489",
+    "url": "/testfolio/static/js/4.7e77a6e9.chunk.js"
+  },
+  {
+    "revision": "c4b6c8c40d88b33563cb",
+    "url": "/testfolio/static/js/main.d0f5fff9.chunk.js"
+  },
+  {
+    "revision": "c168146b05c4337541b0",
+    "url": "/testfolio/static/js/runtime-main.727c562f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/testfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/testfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/testfolio/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/testfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/testfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/testfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/testfolio/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/testfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/testfolio/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/testfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/testfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/testfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/testfolio/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/testfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/testfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/testfolio/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/testfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
